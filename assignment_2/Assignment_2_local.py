4c6ebea472e04a8bc7befc3856db1ab28338b2a7
Marcio Woitek <woitek@usp.br>
2021-01-02 22:08:19 -0300

    Finished 2nd programming assignment

--- assignment_2/Assignment_2_local.py
+++ assignment_2/Assignment_2_local.py
@@ -33,8 +33,10 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, validation_curve
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 
 # %matplotlib notebook
@@ -223,12 +225,12 @@ answer_two()
 def plot_two():
     """Plot the R^2 regression scores computed by the previous function."""
     scores_train, scores_test = answer_two()
-    fig, ax = plt.subplots(figsize=(10, 5))
+    _, ax = plt.subplots(figsize=(10, 5))
     labels = [str(i) for i in range(10)]
     x = np.arange(len(labels))
     width = 0.3
-    rects_1 = ax.bar(x - width / 2, scores_train, width, label='Training Data')
-    rects_2 = ax.bar(x + width / 2, scores_test, width, label='Test Data')
+    ax.bar(x - width / 2, scores_train, width, label='Training Data')
+    ax.bar(x + width / 2, scores_test, width, label='Test Data')
     ax.set_xlabel('Degree')
     ax.set_ylabel('Score')
     ax.set_title('Regression Scores for the Training and Test Sets')
@@ -336,7 +338,6 @@ answer_four()
 # pd.get_dummies to convert the categorical variables into indicator variables.
 
 # %%
-# mush_df = pd.read_csv('readonly/mushrooms.csv')
 mush_df = pd.read_csv('mushrooms.csv')
 mush_df2 = pd.get_dummies(mush_df)
 
@@ -358,41 +359,79 @@ y_subset = y_test2
 # %% [markdown]
 # ### Question 5
 #
-# Using `X_train2` and `y_train2` from the preceding cell, train a DecisionTreeClassifier with default parameters and random_state=0. What are the 5 most important features found by the decision tree?
+# Using `X_train2` and `y_train2` from the preceding cell, train a
+# DecisionTreeClassifier with default parameters and `random_state=0`. What are
+# the 5 most important features found by the decision tree?
 #
-# As a reminder, the feature names are available in the `X_train2.columns` property, and the order of the features in `X_train2.columns` matches the order of the feature importance values in the classifier's `feature_importances_` property.
+# As a reminder, the feature names are available in the `X_train2.columns`
+# property, and the order of the features in `X_train2.columns` matches the
+# order of the feature importance values in the classifier's
+# `feature_importances_` property.
 #
-# *This function should return a list of length 5 containing the feature names in descending order of importance.*
-#
-# *Note: remember that you also need to set random_state in the DecisionTreeClassifier.*
+# *This function should return a list of length 5 containing the feature names
+# in descending order of importance.*
 
 # %%
 def answer_five():
-    from sklearn.tree import DecisionTreeClassifier
 
-    # Your code here
+    # Create a DecisionTreeClassifier object:
+    clf = DecisionTreeClassifier(random_state=0)
 
-    return # Your answer here
+    # Fit this classifier using the training data X_train2 and y_train2:
+    clf.fit(X_train2, y_train2)
 
+    # Get the indexes for putting the feature importances in descending order:
+    idx = clf.feature_importances_.argsort()[::-1]
+
+    # Create the list containing the names of the 5 most important features:
+    top_5 = list(X_train2.columns.values[idx[:5]])
+
+    return top_5
+
+
+answer_five()
 
 # %% [markdown]
 # ### Question 6
 #
-# For this question, we're going to use the `validation_curve` function in `sklearn.model_selection` to determine training and test scores for a Support Vector Classifier (`SVC`) with varying parameter values.  Recall that the validation_curve function, in addition to taking an initialized unfitted classifier object, takes a dataset as input and does its own internal train-test splits to compute results.
+# For this question, we're going to use the `validation_curve` function in
+# `sklearn.model_selection` to determine training and test scores for a Support
+# Vector Classifier (`SVC`) with varying parameter values. Recall that the
+# `validation_curve` function, in addition to taking an initialized unfitted
+# classifier object, takes a dataset as input and does its own internal
+# train-test splits to compute results.
 #
-# **Because creating a validation curve requires fitting multiple models, for performance reasons this question will use just a subset of the original mushroom dataset: please use the variables X_subset and y_subset as input to the validation curve function (instead of X_mush and y_mush) to reduce computation time.**
+# **Because creating a validation curve requires fitting multiple models, for
+# performance reasons this question will use just a subset of the original
+# mushroom dataset: please use the variables `X_subset` and `y_subset` as input
+# to the validation curve function (instead of `X_mush` and `y_mush`) to reduce
+# computation time.**
 #
-# The initialized unfitted classifier object we'll be using is a Support Vector Classifier with radial basis kernel.  So your first step is to create an `SVC` object with default parameters (i.e. `kernel='rbf', C=1`) and `random_state=0`. Recall that the kernel width of the RBF kernel is controlled using the `gamma` parameter.
+# The initialized unfitted classifier object we'll be using is a Support Vector
+# Classifier with radial basis kernel. So your first step is to create an
+# `SVC` object with default parameters (i.e., `kernel='rbf', C=1`) and
+# `random_state=0`. Recall that the kernel width of the RBF kernel is
+# controlled using the `gamma` parameter.
 #
-# With this classifier, and the dataset in X_subset, y_subset, explore the effect of `gamma` on classifier accuracy by using the `validation_curve` function to find the training and test scores for 6 values of `gamma` from `0.0001` to `10` (i.e. `np.logspace(-4,1,6)`). Recall that you can specify what scoring metric you want validation_curve to use by setting the "scoring" parameter.  In this case, we want to use "accuracy" as the scoring metric.
+# With this classifier, and the dataset in `X_subset`, `y_subset`, explore the
+# effect of `gamma` on classifier accuracy by using the `validation_curve`
+# function to find the training and test scores for 6 values of `gamma` from
+# `0.0001` to `10` (i.e., `np.logspace(-4, 1, 6)`). Recall that you can specify
+# what scoring metric you want `validation_curve` to use by setting the
+# "scoring" parameter. In this case, we want to use "accuracy" as the scoring
+# metric.
 #
-# For each level of `gamma`, `validation_curve` will fit 3 models on different subsets of the data, returning two 6x3 (6 levels of gamma x 3 fits per level) arrays of the scores for the training and test sets.
+# For each level of `gamma`, `validation_curve` will fit 3 models on different
+# subsets of the data, returning two 6x3 (6 levels of gamma x 3 fits per level)
+# arrays of the scores for the training and test sets.
 #
-# Find the mean score across the three models for each level of `gamma` for both arrays, creating two arrays of length 6, and return a tuple with the two arrays.
+# Find the mean score across the three models for each level of `gamma` for
+# both arrays, creating two arrays of length 6, and return a tuple with the two
+# arrays.
 #
-# e.g.
+# e.g.,
 #
-# if one of your array of scores is
+# if one of your arrays of scores is
 #
 #     array([[ 0.5,  0.4,  0.6],
 #            [ 0.7,  0.8,  0.7],
@@ -405,30 +444,85 @@ def answer_five():
 #
 #     array([ 0.5,  0.73333333,  0.83333333,  0.76666667,  0.63333333, 0.5])
 #
-# *This function should return one tuple of numpy arrays `(training_scores, test_scores)` where each array in the tuple has shape `(6,)`.*
+# *This function should return one tuple of numpy arrays `(training_scores, test_scores)`
+# where each array in the tuple has shape `(6,)`.*
 
 # %%
 def answer_six():
-    from sklearn.svm import SVC
-    from sklearn.model_selection import validation_curve
 
-    # Your code here
+    # Create an SVC object:
+    clf = SVC(kernel='rbf', C=1, random_state=0)
 
-    return # Your answer here
+    # Create the array containing the values of the gamma parameter:
+    gammas = np.logspace(-4, 1, 6)
 
+    # Compute the training and test scores for the values of gamma specified above:
+    train_scores, test_scores = validation_curve(
+        estimator=clf,
+        X=X_subset,
+        y=y_subset,
+        param_name='gamma',
+        param_range=gammas,
+        cv=3,
+        scoring='accuracy'
+    )
+
+    # For each value of gamma, compute the mean score corresponding to the training data:
+    train_means = train_scores.mean(axis=1)
+
+    # For each value of gamma, compute the mean score corresponding to the test data:
+    test_means = test_scores.mean(axis=1)
+
+    return (train_means, test_means)
+
+
+answer_six()
 
 # %% [markdown]
 # ### Question 7
 #
-# Based on the scores from question 6, what gamma value corresponds to a model that is underfitting (and has the worst test set accuracy)? What gamma value corresponds to a model that is overfitting (and has the worst test set accuracy)? What choice of gamma would be the best choice for a model with good generalization performance on this dataset (high accuracy on both training and test set)?
+# Based on the scores from question 6, what gamma value corresponds to a model
+# that is underfitting (and has the worst test set accuracy)? What gamma value
+# corresponds to a model that is overfitting (and has the worst test set
+# accuracy)? What choice of gamma would be the best choice for a model with
+# good generalization performance on this dataset (high accuracy on both
+# training and test set)?
 #
-# Hint: Try plotting the scores from question 6 to visualize the relationship between gamma and accuracy. Remember to comment out the import matplotlib line before submission.
+# Hint: Try plotting the scores from question 6 to visualize the relationship
+# between gamma and accuracy. Remember to comment out the `import matplotlib`
+# line before submission.
 #
-# *This function should return one tuple with the degree values in this order: `(Underfitting, Overfitting, Good_Generalization)` Please note there is only one correct solution.*
+# *This function should return one tuple with the degree values in this order:
+# `(Underfitting, Overfitting, Good_Generalization)`. Please note there is only
+# one correct solution.*
 
 # %%
+def plot_six():
+    """Plot the mean training and test scores computed by the previous function."""
+    train_means, test_means = answer_six()
+    _, ax = plt.subplots(figsize=(10, 5))
+    labels = [str(gamma) for gamma in np.logspace(-4, 1, 6)]
+    x = np.arange(len(labels))
+    width = 0.3
+    ax.bar(x - width / 2, train_means, width, label='Training Data')
+    ax.bar(x + width / 2, test_means, width, label='Test Data')
+    ax.set_xlabel('gamma')
+    ax.set_ylabel('Mean score')
+    ax.set_title('Mean Scores for the Training and Test Sets')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    plt.show()
+
+
+plot_six()
+
+
 def answer_seven():
 
-    # Your code here
+    gammas = np.logspace(-4, 1, 6)
+    underfitting = gammas[0]
+    overfitting = gammas[5]
+    good_generalization = gammas[3]
 
-    return # Return your answer
+    return (underfitting, overfitting, good_generalization)
